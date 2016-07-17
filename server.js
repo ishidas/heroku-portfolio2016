8ba8b2af67ecc618c3826b6cc16a83508c5d1721
Sawako Ishida <sawako.yoriko@gmail.com>
2016-07-17 14:37:22 -0700

    changed server.js to go thru /api and added process.env.port to listen

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const gzippo = require('gzippo');
 
 //db set up
-const PORT = process.env.MONGO_PORT || 3000;
+// const PORT = process.env.MONGO_PORT || 3000;
 // const DB = process.env.MONGO_URI || 'mongodb://localhost/db';
 // const test = 'mongodb://localhost/db';
 mongoose.connect(process.env.MONGODB_URI);
@@ -20,27 +20,27 @@ require(__dirname + '/models/blog-model.js')(mongoose, models);
 app.use(bodyParser.json());
 
 app.use((req, res, next)=>{
-  res.header('Access-Control-Allow-Origin','http://localhost:8080');
+  res.header('Access-Control-Allow-Origin','https://github.com/ishidas/heroku-jan2016-portfolio');
   res.header('Access-Control-Allow-Headers','Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   next();
 });
 
-app.get('/blog/articles', (req, res)=>{
+app.get('api/blog/articles', (req, res)=>{
   models.Blog.find({},(err, blog)=>{
     if(err) res.json({error: err});
     res.json(blog);
   });
 });
 
-app.get('/blog/articles/:id', (req, res)=>{
+app.get('api/blog/articles/:id', (req, res)=>{
   models.Blog.findOne({_id: req.params.id},(err, blog)=>{
     if(err) return res.json({error: err});
     res.json(blog);
   });
 });
 
-app.post('/admin/blog', (req, res)=>{
+app.post('api/admin/blog', (req, res)=>{
   console.log('rea.body : ' + JSON.stringify(req.body));
   var newBlog = new models.Blog(req.body);
 
@@ -54,18 +54,18 @@ app.post('/admin/blog', (req, res)=>{
   });
 });
 
-app.put('/admin/blog/article/:id', (req, res)=>{
+app.put('api/admin/blog/article/:id', (req, res)=>{
   models.Blog.findOneAndUpdate({_id: req.params.id},{$set: req.body}, {new: true},(err, updatedBlog)=>{
     if(err) return res.json({error: err});
     res.json(updatedBlog);
   });
 });
 
-app.delete('/admin/blog/article/:id', (req, res)=>{
+app.delete('api/admin/blog/article/:id', (req, res)=>{
   models.Blog.findOneAndRemove({_id: req.params.id}, (err)=>{
     if(err) return res.json({error: err});
     res.json({msg: 'Success!'});
   });
 });
 
-app.listen(PORT, ()=> console.log('Backend server listening at 3000'));
+app.listen(process.env.PORT, ()=> console.log('Backend server listening at 3000'));
