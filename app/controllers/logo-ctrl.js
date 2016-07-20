21eb7491dc0a6124069fca563418ea7875411fff
Sawako Ishida <sawako.yoriko@gmail.com>
2016-07-19 19:23:26 -0700

    changed to using hasClass

--- app/controllers/logo-ctrl.js
+++ app/controllers/logo-ctrl.js
@@ -3,7 +3,7 @@ const angular = require('angular');
 module.exports = function(app, angular){
   app.controller('logoController',['$document','$window', function($document, $window){
 
-    console.log('finding section', angular.element('section').find('non-sticky'));
+    console.log('finding section', angular.element('section').hasClass('non-sticky'));
 
 
   }]);
