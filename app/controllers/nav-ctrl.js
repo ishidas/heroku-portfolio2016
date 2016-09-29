94fcd83b3990656ed225837c0fb4efe583eaf94a
Sawako <sawako.yoriko@gmail.com>
2016-09-29 16:18:05 -0700

    -mtrying to merge back to master with prev commit

--- app/controllers/nav-ctrl.js
+++ app/controllers/nav-ctrl.js
@@ -1,8 +1,8 @@
-'use strict';
-module.exports = function(app){
-  app.controller('navController', function(){
-    this.clickable =  function(index){
-      console.log('index??', index);
-    };
-  });
-};
+// 'use strict';
+// module.exports = function(app){
+//   app.controller('navController', function(){
+//     this.clickable =  function(index){
+//       console.log('index??', index);
+//     };
+//   });
+// };
